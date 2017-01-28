607008fdc1a231a678afccc8fee90a8b8f6a06c9
Marcos Costa <marcos.lcosta@live.com>
2017-01-28 16:54:33 -0300

    Curso Angular 2 #42: Injetando um serviço em outro serviço

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 import { CursosModule } from './cursos/cursos.module';
 
+import { LogService } from './shared/log.service';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +21,7 @@ import { CursosModule } from './cursos/cursos.module';
     CriarCursoModule,
     CursosModule
   ],
-  providers: [],
+  providers: [LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
