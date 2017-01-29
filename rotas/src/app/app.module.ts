f8b9b67c6fa5a6288f9418dd562ffbfece792a11
Marcos Costa <marcos.lcosta@live.com>
2017-01-29 11:15:25 -0300

    Curso Angular 2 #50: Rotas: Configurando rotas simples

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { MaterializeModule } from 'angular2-materialize';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -7,18 +9,21 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    //MaterializeModule,
     LoginComponent,
-    CursosComponent
+    CursosComponent    
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
