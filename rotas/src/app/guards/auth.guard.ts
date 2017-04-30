6f6d6642d26bb557509c11d6fc2ce33618062f43
Marcos Costa <marcos.lcosta@live.com>
2017-04-30 14:54:12 -0300

    usando CanLoad

--- rotas/src/app/guards/auth.guard.ts
+++ rotas/src/app/guards/auth.guard.ts
@@ -5,14 +5,16 @@ import {
   CanActivate
   , Router
   , ActivatedRouteSnapshot
-  , RouterStateSnapshot
+  , RouterStateSnapshot,
+  CanLoad,
+  Route
 } from '@angular/router';
 
 import { AuthService } from './../login/auth.service';
 
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(
     private authService: AuthService,
@@ -26,13 +28,25 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | boolean {
-    if (this.authService.usuarioEstaAutenticado()) {
+   return this.verificarAcesso();
+  }
+
+  private verificarAcesso():boolean{
+    console.log("AuthGuard:  verificarAcesso  ")
+
+     if (this.authService.usuarioEstaAutenticado()) {
       return true;
     }
     else {
       this.router.navigate(['/login'])
     }
+    return false;
   }
+
+  	canLoad(route: Route): Observable<boolean>|Promise<boolean>|boolean {
+     console.log("canLoad:  verificarAcesso se usuário pode carregar o codigo do modulo  ")
+      return this.verificarAcesso();
+    }
 
 
 }
